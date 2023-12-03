c1d3eefc499acefaab545723511fc54ed353e4dc
Sammy Hasan <sammy.nm.hasan@gmail.com>
2023-12-03 11:44:33 +0000

    Add day 3 stage 2 solution

--- 03/solve.py
+++ 03/solve.py
@@ -1,9 +1,7 @@
-
-import os
-
 from typing import List
 
 from rich.console import Console
+
 console = Console()
 
 test_case = [
@@ -58,12 +56,51 @@ def solve(matrix):
                         or snapshot_middle[0] != "."
                         or snapshot_middle[-1] != "."
                     ):
-                        
                         numbers_register.append(int(digits))
 
                     digits = ""
 
     return numbers_register
+
+
+def solve_2(matrix):
+    numbers_map = {}
+    star_indices = set()
+    gear_ratios = 0
+
+    for row_ix in range(1, len(matrix)):
+        digits = ""
+
+        for col_ix in range(1, len(matrix[0])):
+            middle = matrix[row_ix][col_ix]
+            if middle.isnumeric():
+                digits += middle
+
+            else:
+                if middle == "*":
+                    star_indices.add((row_ix, col_ix))
+
+                if digits:
+                    for cix in range(col_ix - len(digits), col_ix):
+                        numbers_map[(row_ix, cix)] = int(digits)
+
+                    digits = ""
+
+    for rix, cix in star_indices:
+        gears = []
+        for rix_shift in [-1, 0, 1]:
+            for cix_shift in [-1, 0, 1]:
+                if (rix + rix_shift, cix + cix_shift) in numbers_map:
+                    gear = numbers_map[rix + rix_shift, cix + cix_shift]
+                    if gear not in gears:
+                        gears.append(gear)
+
+        if len(gears) == 2:
+            gear_ratios += gears[0] * gears[1]
+        elif len(gears) > 2:
+            raise RuntimeError("Ooops!")
+
+    return gear_ratios
 
 
 def main() -> None:
@@ -73,11 +110,12 @@ def main() -> None:
 
     padded_matrix = extract_matrix(lines)
     print(*padded_matrix, sep="\n")
-    
+
     sol = solve(padded_matrix)
-    # print(sol)
-    
     print("STAGE 1: ", sum(sol))
+
+    stage_2 = solve_2(padded_matrix)
+    print("STAGE 2:", stage_2)
 
 
 if __name__ == "__main__":
